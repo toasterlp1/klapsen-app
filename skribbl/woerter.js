e1259adf3d66373c7ad336f9039b86fbffe3a10d
toasterlp1 <ihmlbto@gmail.com>
2026-07-20 22:01:42 +0200

    Add files via upload

---
+++ skribbl/woerter.js
@@ -0,0 +1,56 @@
+
+const WOERTER = [
+
+  "Elefant","Giraffe","Pinguin","Krokodil","Fledermaus","Schnecke","Igel","Eichhörnchen",
+  "Delfin","Qualle","Oktopus","Hai","Schmetterling","Marienkäfer","Spinne","Biene",
+  "Frosch","Schildkröte","Kamel","Faultier","Nashorn","Waschbär","Papagei","Pfau",
+  "Seepferdchen","Skorpion","Chamäleon","Wal","Adler","Eule",
+
+  "Wasserhahn","Regenschirm","Staubsauger","Bügeleisen","Wäscheklammer","Zahnbürste",
+  "Kerzenständer","Türklinke","Steckdose","Glühbirne","Waschmaschine","Kühlschrank",
+  "Mikrowelle","Toaster","Wecker","Sanduhr","Lupe","Schere","Kleiderbügel","Besen",
+  "Mülleimer","Spiegel","Teppich","Vorhang","Kissen","Leiter","Werkzeugkasten","Hammer",
+  "Schraubenzieher","Taschenlampe",
+
+  "Ananas","Wassermelone","Brezel","Croissant","Spiegelei","Hamburger","Pizza","Popcorn",
+  "Eistüte","Lutscher","Donut","Spaghetti","Sushi","Hotdog","Kaugummi","Zuckerwatte",
+  "Kürbis","Karotte","Champignon","Chilischote","Erdbeere","Banane","Käse","Baguette",
+
+  "Leuchtturm","Windmühle","Iglu","Zelt","Burg","Kirche","Brücke","Wolkenkratzer",
+  "Achterbahn","Riesenrad","Pyramide","Vulkan","Wasserfall","Insel","Höhle","Bauernhof",
+  "Bushaltestelle","Tankstelle","Schwimmbad","Bibliothek",
+
+  "Heissluftballon","U-Boot","Traktor","Feuerwehrauto","Segelboot","Skateboard","Rollstuhl",
+  "Motorrad","Hubschrauber","Rakete","Zeppelin","Seilbahn","Einkaufswagen","Schubkarre",
+  "Bagger","Panzer","Fahrrad","Strassenbahn",
+
+  "Zylinder","Krawatte","Handschuh","Gummistiefel","Sonnenbrille","Rucksack","Schal",
+  "Bikini","Anzug","Turnschuh","Krone","Helm","Regenmantel","Fliege",
+
+  "Angelrute","Dartscheibe","Schach","Trampolin","Skier","Boxhandschuh","Basketballkorb",
+  "Tennisschläger","Golfschläger","Bowling","Fallschirm","Surfbrett","Hantel","Springseil",
+
+  "Gitarre","Schlagzeug","Trompete","Klavier","Geige","Harfe","Mikrofon","Kopfhörer",
+  "Plattenspieler","Triangel",
+
+  "Regenbogen","Blitz","Tornado","Kaktus","Pilz","Sonnenblume","Tannenbaum","Schneemann",
+  "Lagerfeuer","Sternschnuppe","Mondsichel","Wolke","Lawine","Wüste",
+
+  "Einhorn","Drache","Meerjungfrau","Zauberstab","Geist","Vampir","Werwolf","Roboter",
+  "Alien","Ufo","Ritter","Pirat","Hexe","Zombie","Yeti","Kraken",
+
+  "Feuerwehrmann","Astronaut","Koch","Clown","Taucher","Cowboy","Ninja","Detektiv",
+  "Briefträger","Bauarbeiter","Zauberer","Superheld",
+
+  "Skelett","Gehirn","Herz","Zahn","Auge","Fussabdruck","Handabdruck","Bart",
+
+  "Fernbedienung","Kompass","Fernrohr","Waage","Thermometer","Batterie","Zahnrad",
+  "Schlüssel","Vorhängeschloss","Briefmarke","Landkarte","Globus",
+
+  "Geschenk","Torte","Luftballon","Konfetti","Pokal","Medaille","Diamant","Schatztruhe",
+  "Angelhaken","Bumerang","Domino","Puzzle","Würfel","Spielkarte","Zirkuszelt","Karussell",
+  "Ampel","Verkehrsschild","Wegweiser","Vogelscheuche","Bienenstock","Spinnennetz",
+  "Hufeisen","Anker","Fahne","Kaugummiautomat","Parkuhr","Litfasssäule"
+];
+
+if (typeof window !== 'undefined') window.WOERTER = WOERTER;
---
+++ stadtlandfluss/kategorien.js
@@ -0,0 +1,38 @@
+
+// Stadt-Land-Fluss — verfuegbare Kategorien
+// Der Leader waehlt vor dem Spiel aus, welche gespielt werden.
+const SLF_KATEGORIEN = [
+  "Stadt",
+  "Land",
+  "Fluss",
+  "Name",
+  "Tier",
+  "Beruf",
+  "Pflanze",
+  "Essen / Getraenk",
+  "Marke",
+  "Film / Serie",
+  "Band / Musiker",
+  "Videospiel",
+  "Anime / Manga",
+  "Youtuber / Streamer",
+  "Koerperteil",
+  "Farbe",
+  "Sportart",
+  "Auto-Marke",
+  "Superheld",
+  "Getraenk",
+  "Suessigkeit",
+  "Klamotten / Mode",
+  "Beruehmte Person",
+  "Serienheld / Charakter",
+  "Etwas das man im Haushalt findet",
+  "Schulfach",
+  "Hauptstadt",
+  "Insel",
+  "Gebirge / Berg",
+  "Musik-Genre"
+];
+
+// Diese Kategorien sind standardmaessig vorausgewaehlt
+const SLF_STANDARD = ["Stadt", "Land", "Fluss", "Name", "Tier", "Beruf"];
