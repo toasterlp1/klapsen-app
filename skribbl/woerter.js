5c02b00185efb74d69807836de7a4ad165ace51a
toasterlp1 <ihmlbto@gmail.com>
2026-07-21 00:09:58 +0200

    Update woerter.js

--- skribbl/woerter.js
+++ skribbl/woerter.js
@@ -50,7 +50,71 @@ const WOERTER = [
   "Geschenk","Torte","Luftballon","Konfetti","Pokal","Medaille","Diamant","Schatztruhe",
   "Angelhaken","Bumerang","Domino","Puzzle","Würfel","Spielkarte","Zirkuszelt","Karussell",
   "Ampel","Verkehrsschild","Wegweiser","Vogelscheuche","Bienenstock","Spinnennetz",
-  "Hufeisen","Anker","Fahne","Kaugummiautomat","Parkuhr","Litfasssäule"
+  "Hufeisen","Anker","Fahne","Kaugummiautomat","Parkuhr","Litfasssäule",
+  
+  // ===== Tiere =====
+  "Wal","Koala","Erdmännchen","Flamingo","Nilpferd","Otter","Alpaka","Tintenfisch",
+  "Ameise","Käfer","Storch","Möwe","Rentier","Wildschwein","Maulwurf","Hummer",
+  "Seestern","Robbe","Gepard","Zebra","Panda","Luchs","Dachs","Bison",
+
+  // ===== Essen und Trinken =====
+  "Croissant","Lasagne","Sushi","Waffel","Popcorn","Brezel","Kürbis","Ananas",
+  "Wassermelone","Spaghetti","Hamburger","Muffin","Donut","Bratwurst","Käse",
+  "Cocktail","Teekanne","Milchshake","Salatschüssel","Grillspiess",
+
+  // ===== Gegenstände =====
+  "Regenschirm","Staubsauger","Nähmaschine","Kompass","Fernglas","Bügeleisen",
+  "Wecker","Kerzenständer","Werkzeugkasten","Gartenschlauch","Leiter","Besen",
+  "Koffer","Rucksack","Taschenlampe","Schere","Locher","Waage","Teleskop","Mikroskop",
+
+  // ===== Orte und Gebäude =====
+  "Leuchtturm","Windmühle","Iglu","Zelt","Schloss","Bahnhof","Kirche","Brücke",
+  "Wolkenkratzer","Bauernhof","Tankstelle","Schwimmbad","Zirkuszelt","Bibliothek",
+  "Achterbahn","Skipiste","Hafen","Tunnel","Aussichtsturm","Baumhaus",
+
+  // ===== Fahrzeuge =====
+  "Heissluftballon","U-Boot","Traktor","Seilbahn","Segelboot","Feuerwehrauto",
+  "Schneemobil","Rikscha","Zeppelin","Skateboard","Einrad","Bagger","Kran",
+  "Rennwagen","Wohnwagen","Kutsche","Jetski","Panzer",
+
+  // ===== Natur =====
+  "Vulkan","Wasserfall","Regenbogen","Blitz","Tornado","Kaktus","Palme","Pilz",
+  "Sonnenblume","Schneeflocke","Lagerfeuer","Sanduhr","Insel","Höhle","Gletscher",
+  "Korallenriff","Wüste","Sumpf","Geysir","Sternschnuppe",
+
+  // ===== Berufe =====
+  "Feuerwehrmann","Astronaut","Koch","Clown","Taucher","Imker","Ritter","Pirat",
+  "Cowboy","Zauberer","Detektiv","Bäcker","Gärtner","Schornsteinfeger","Dirigent",
+  "Bergsteiger","Schiedsrichter","Fotograf","Maler","Bademeister",
+
+  // ===== Sport =====
+  "Bogenschiessen","Curling","Turmspringen","Skispringen","Klettern","Rudern",
+  "Fechten","Tischtennis","Basketballkorb","Torwart","Hantel","Trampolin",
+  "Stabhochsprung","Boxhandschuh","Golfschläger","Surfbrett","Schlittschuh",
+
+  // ===== Abstraktes und Redewendungen =====
+  "Heimweh","Schadenfreude","Fernweh","Schnappsidee","Zeitdruck","Glückssträhne",
+  "Ohrwurm","Torschlusspanik","Sandmännchen","Luftschloss","Drahtseilakt",
+  "Nadel im Heuhaufen","Sturm im Wasserglas","Eselsbrücke","Rampenlicht",
+
+  // ===== Filme, Spiele, Kultur =====
+  "Zeitmaschine","Raumschiff","Roboter","Drache","Einhorn","Meerjungfrau","Yeti",
+  "Vampir","Zombie","Geist","Superheld","Ninja","Samurai","Wikinger","Mumie",
+  "Schatzkarte","Kristallkugel","Zauberstab","Zaubertrank","Thron",
+
+  // ===== Alltag =====
+  "Wäscheleine","Geschirrspüler","Mikrowelle","Bügelbrett","Mülltonne","Briefkasten",
+  "Türklingel","Fussmatte","Blumentopf","Spiegel","Hängematte","Schaukelstuhl",
+  "Kaffeemaschine","Wasserkocher","Toaster","Kühlschrank","Waschmaschine",
+  "Nachttisch","Kleiderbügel","Schuhregal",
+
+  // ===== Musik =====
+  "Schlagzeug","Dudelsack","Harfe","Akkordeon","Trompete","Geige","Mikrofon",
+  "Plattenspieler","Kopfhörer","Notenblatt","Konzertbühne","Lautsprecher",
+
+  // ===== Kleidung =====
+  "Zylinder","Gummistiefel","Fliege","Handschuh","Schal","Sonnenbrille","Krone",
+  "Fliegerbrille","Badeanzug","Regenmantel","Hausschuhe","Halskette","Armbanduhr"
 ];
 
 if (typeof window !== 'undefined') window.WOERTER = WOERTER;
