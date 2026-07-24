85bd5e5f286d26c607a00633a4edba2968de5248
toasterlp1 <ihmlbto@gmail.com>
2026-07-25 00:34:37 +0200

    Update avatare.js

--- avatare.js
+++ avatare.js
@@ -4,7 +4,7 @@ const AVATARE = {
   "ügelibügeli": "https://cdn.discordapp.com/avatars/758271488045350912/12300b63fc57de58b01a3a3408a21f80.webp?size=256",
   "cijäy": "https://cdn.discordapp.com/avatars/756944039160578129/b27f04ad8e3d620d1a0760ac277e6543.webp?size=256",
   "töbi": "https://cdn.discordapp.com/avatars/205076563593527297/0956f387e30864f0b795d99c8d1ff34b.webp?size=256",
-  "töster": "https://cdn.discordapp.com/avatars/809064080790388788/363545deb70181b55f6ee50ddc77f6d0.webp?size=256",
+  "töster": "https://cdn.discordapp.com/avatars/809064080790388788/c35e2e93adb83e54de9b308fe5a37c9a.webp?size=256",
   "älex": "https://cdn.discordapp.com/avatars/1123315501205045358/303743e9efe0d3a644cada7957c9fcfe.webp?size=256",
 };
 function avatarFor(name){
