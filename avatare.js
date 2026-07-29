464515f1a4e428e7433ff90520b5b5e5cc091f1c
toasterlp1 <ihmlbto@gmail.com>
2026-07-29 18:10:58 +0200

    Update avatare.js

--- avatare.js
+++ avatare.js
@@ -1,12 +1,19 @@
-//Für Jannikgruppe man man
+
+const GRUPPE = "movehaengers";
+
 const AVATARE = {
-  "säule": "https://cdn.discordapp.com/avatars/514145088822181908/86c56c968daead276bd60db8473249ce.webp?size=256",
-  "ügelibügeli": "https://cdn.discordapp.com/avatars/758271488045350912/12300b63fc57de58b01a3a3408a21f80.webp?size=256",
-  "cijäy": "https://cdn.discordapp.com/avatars/756944039160578129/b27f04ad8e3d620d1a0760ac277e6543.webp?size=256",
-  "töbi": "https://cdn.discordapp.com/avatars/205076563593527297/0956f387e30864f0b795d99c8d1ff34b.webp?size=256",
-  "töster": "https://cdn.discordapp.com/avatars/809064080790388788/c35e2e93adb83e54de9b308fe5a37c9a.webp?size=256",
-  "älex": "https://cdn.discordapp.com/avatars/1123315501205045358/303743e9efe0d3a644cada7957c9fcfe.webp?size=256",
+  "emooo": "https://cdn.discordapp.com/avatars/1021789366575710260/4f346707e65c4fd4b4a3d115b8755280.webp?size=256",
+  "luuu": "https://cdn.discordapp.com/avatars/769606009508855898/4bb654e1e7b8f44ee0cc348a78ff2bd6.webp?size=256",
+  "lone": "https://cdn.discordapp.com/avatars/1292716508337799299/8b8663401756e14adeec15c9fa95cc3e.webp?size=256",
+  "pacman": "https://cdn.discordapp.com/avatars/1173295394072039455/8ce92afedaae3e4fe5ac21489b71fdb3.webp?size=256",
+  "smöökinghigh": "https://cdn.discordapp.com/avatars/1176925420071956672/8baa6d927b244b833fb52e57115a2710.webp?size=256",
+  "mistertöby": "https://cdn.discordapp.com/avatars/574983072542621707/056428d7ff354d303d260a3499663399.webp?size=256",
+  "införnäl": "https://cdn.discordapp.com/avatars/428143445836824576/e359f86e8c6ee217a286c5d2b4b147e4.webp?size=256",
+  "töster": "https://cdn.discordapp.com/avatars/809064080790388788/363545deb70181b55f6ee50ddc77f6d0.webp?size=256"
 };
 function avatarFor(name){
   return AVATARE[name] || null;
+}
+function aktuelleGruppe(){
+  return (typeof GRUPPE !== 'undefined' && GRUPPE) ? GRUPPE : 'standard';
 }
